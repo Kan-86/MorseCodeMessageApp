36f394d496e11b174d870810c2c457a3c5c877d1
Sub-Z3r0 <andersen19@gmail.com>
2019-01-30 10:11:51 +0100

    something for masta

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBk7ZCBUBg0bQj6AnQ_pD1WZ7QCpKgcGxA",
+    authDomain: "advancedwebappproject.firebaseapp.com",
+    databaseURL: "https://advancedwebappproject.firebaseio.com",
+    projectId: "advancedwebappproject",
+    storageBucket: "",
+    messagingSenderId: "32543920072"
+  }
+};
